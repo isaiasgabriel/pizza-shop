1c49f4644bb98e0889dd7723606d463beae094e5
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-11-18 17:08:31 -0300

    📦️ Shadcn/ui installation and configuration
    https://ui.shadcn.com/docs/installation/vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
 export function App() {
   return (
-    <h1>Hello world</h1>
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Send</Button>
+    </div>
   )
 }
 
