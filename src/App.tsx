491f0ee03f410ee8c85e6b51f26523686cdbcc58
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-11-19 13:57:53 -0300

    📦️ React helmet async

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './global.css'
 
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
 import { router } from './routes.tsx'
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <HelmetProvider>
+      {/* Sets the page title dynamically using %s to represent the current page name. */}
+      <Helmet titleTemplate="%s | Pizza Shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
 }
