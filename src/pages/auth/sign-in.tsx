c58b525f7a15d7c7e544c8fc3964472d612332c7
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-11-19 14:41:20 -0300

    ✨ Login page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,36 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <h1>Sign-in</h1>
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Access Dashboard
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Track your sales through the partner panel
+            </p>
+          </div>
+
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Your email:</Label>
+              <Input id="email" type="email"></Input>
+            </div>
+
+            <Button className="w-full" type="submit">
+              Access
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
