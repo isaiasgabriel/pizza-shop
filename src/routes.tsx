a16f587831ce3c7926d70e0dc9adfc10ffd9250a
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-11-19 12:49:05 -0300

    🧱 Routes layout with Outlet

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { AppLayout } from './pages/_layouts/appLayout'
+import { AuthLayout } from './pages/_layouts/authLayout'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Dashboard />,
+    element: <AppLayout />,
+    children: [{ path: '/', element: <Dashboard /> }],
   },
   {
-    path: 'sign-in',
-    element: <SignIn />,
+    path: '/sign-in',
+    element: <AuthLayout />,
+    children: [{ path: '/sign-in', element: <SignIn /> }],
   },
 ])
