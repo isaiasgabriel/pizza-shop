1d908e9a1170673148e59528a417f97dceb221e8
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-11-22 14:03:56 -0300

    💄 Theme toggle
    https://ui.shadcn.com/docs/dark-mode/vite
    
    1. Create a theme provider > theme-provider.tsx
    2. Wrap your root layout with the theme provider > App.tsx
    3.  Create a theme toggle > theme-toggle.tsx
    4. Add this toggle to your header > header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider.tsx'
 import { router } from './routes.tsx'
 
 export function App() {
   return (
-    <HelmetProvider>
-      {/* Sets the page title dynamically using %s to represent the current page name. */}
-      <Helmet titleTemplate="%s | Pizza Shop" />
-      <Toaster richColors />
-      <RouterProvider router={router} />
-    </HelmetProvider>
+    <ThemeProvider defaultTheme="system" storageKey="pizzashop-theme">
+      <HelmetProvider>
+        {/* Sets the page title dynamically using %s to represent the current page name. */}
+        <Helmet titleTemplate="%s | Pizza Shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </ThemeProvider>
   )
 }
