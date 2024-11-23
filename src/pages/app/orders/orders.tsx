184743279df8856743779f8d63055d69537f12f7
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-11-22 22:38:07 -0300

    ✨ Orders pagination

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Pagination } from '@/components/pagination'
 import {
   Table,
   TableBody,
@@ -49,6 +50,7 @@ export function Orders() {
             </TableBody>
           </Table>
         </div>
+        <Pagination pageIndex={0} totalCount={105} perPage={10} />
       </div>
     </>
   )
