ecb1774c011e44fc8960fd3a30bcdd586471a297
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-11-22 22:44:34 -0300

    🐛 Correct closing of div to allow proper styling

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -19,38 +19,38 @@ export function Orders() {
       <Helmet title="Orders" /* Tab Title */ />
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold -tracking-tight">Orders</h1>
-      </div>
 
-      {/* Content */}
-      <div
-        className="space-y-4"
-        /* space-y-2.5 > margin-top: 0.625rem > 10px */
-      >
-        {/* Search */}
-        <OrderTableFilters />
-        {/* Table */}
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[64px]"></TableHead>
-                <TableHead className="w-[325px]">Identifier</TableHead>
-                <TableHead className="w-[180px]">Time passed</TableHead>
-                <TableHead className="w-[140px]">Status</TableHead>
-                <TableHead>Client</TableHead>
-                <TableHead className="w-[140px]">Total</TableHead>
-                <TableHead className="w-[100px]"></TableHead>
-                <TableHead className="w-[100px]"></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {Array.from({ length: 10 }).map((_, i) => {
-                return <OrderTableRow key={i} />
-              })}
-            </TableBody>
-          </Table>
+        {/* Content */}
+        <div
+          className="space-y-4"
+          /* space-y-2.5 > margin-top: 0.625rem > 10px */
+        >
+          {/* Search */}
+          <OrderTableFilters />
+          {/* Table */}
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[325px]">Identifier</TableHead>
+                  <TableHead className="w-[180px]">Time passed</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Client</TableHead>
+                  <TableHead className="w-[140px]">Total</TableHead>
+                  <TableHead className="w-[100px]"></TableHead>
+                  <TableHead className="w-[100px]"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, i) => {
+                  return <OrderTableRow key={i} />
+                })}
+              </TableBody>
+            </Table>
+          </div>
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
-        <Pagination pageIndex={0} totalCount={105} perPage={10} />
       </div>
     </>
   )
