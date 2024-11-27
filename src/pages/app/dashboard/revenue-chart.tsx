5830c5d3070675d71398c6c7052a210e4f166c04
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-11-27 17:45:39 -0300

    📈 Basic chart example using Recharts

--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -1,9 +1,30 @@
 import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
+
+import {
   Card,
+  CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+
+const data = [
+  { date: '10/12', revenue: 900 },
+  { date: '11/12', revenue: 1100 },
+  { date: '12/12', revenue: 1000 },
+  { date: '13/12', revenue: 800 },
+  { date: '14/12', revenue: 700 },
+  { date: '15/12', revenue: 1500 },
+  { date: '16/12', revenue: 2000 },
+]
 
 export function RevenueChart() {
   return (
@@ -16,6 +37,14 @@ export function RevenueChart() {
           <CardDescription>Daily revenue in the period</CardDescription>
         </div>
       </CardHeader>
+
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <Line type="linear" strokeWidth={2} dataKey="revenue"></Line>
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
     </Card>
   )
 }
