e406b8a9f921dd5466f90b8d3e9e1514af2ea705
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-11-29 08:41:55 -0300

    📊 Pie chart customization

--- src/pages/app/dashboard/popular-products-chart.tsx
+++ src/pages/app/dashboard/popular-products-chart.tsx
@@ -1,14 +1,23 @@
 import { BarChart } from 'lucide-react'
-import { Pie, PieChart, ResponsiveContainer } from 'recharts'
+import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts'
+import colors from 'tailwindcss/colors'
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 const data = [
-  { product: 'Chesse', amount: 900 },
-  { product: 'Broccoli', amount: 1100 },
-  { product: 'Pepperoni', amount: 1000 },
-  { product: 'Margherita', amount: 800 },
-  { product: 'Hawaiian', amount: 700 },
+  { product: 'Chesse', amount: 40 },
+  { product: 'Broccoli', amount: 30 },
+  { product: 'Pepperoni', amount: 50 },
+  { product: 'Margherita', amount: 20 },
+  { product: 'Hawaiian', amount: 10 },
+]
+
+const COLORS = [
+  colors.violet[500],
+  colors.sky[500],
+  colors.amber[500],
+  colors.emerald[500],
+  colors.rose[500],
 ]
 
 export function PopularProductsChart() {
@@ -26,7 +35,28 @@ export function PopularProductsChart() {
       <CardContent>
         <ResponsiveContainer width="100%" height={240}>
           <PieChart style={{ fontSize: 12 }}>
-            <Pie data={data} dataKey="amount" nameKey="product" />
+            <Pie
+              data={data}
+              dataKey="amount"
+              nameKey="product"
+              cx="50%"
+              cy="50%"
+              /* outerRadius and innerRadius needs each other to work */
+              /* it basically create a ring-shaped chart with nothing inside */
+              outerRadius={86}
+              innerRadius={64}
+              strokeWidth={8} /* Border width */
+            >
+              {data.map((_, index) => {
+                return (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index]}
+                    className="stroke-background hover:opacity-80" // applies the background color to the border
+                  />
+                )
+              })}
+            </Pie>
           </PieChart>
         </ResponsiveContainer>
       </CardContent>
