51c7b0043e5b4b38c06f13ae64703cb6e4fa3d56
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-11-29 14:06:42 -0300

    ⚙️ React query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/theme/theme-provider.tsx'
+import { queryClient } from './lib/react-query.ts'
 import { router } from './routes.tsx'
 
 export function App() {
@@ -14,7 +16,9 @@ export function App() {
         {/* Sets the page title dynamically using %s to represent the current page name. */}
         <Helmet titleTemplate="%s | Pizza Shop" />
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </HelmetProvider>
     </ThemeProvider>
   )
