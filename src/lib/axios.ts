54f2465f6801ed8ec5fa29e0eb139b3e7c2d332c
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-11-29 18:19:52 -0300

    ⏳ Simulate slow API for loading components tests

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,3 +6,15 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true,
 })
+
+// It basically checks if the API delay is true
+// then it creates a new promise that lasts 2 seconds
+// this way we can test loading components of the page
+if (env.VITE_ENABLE_API_DELAY) {
+  // Before every API call , it'll execute the function below:
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    return config // config is the body of the API call
+  })
+}
