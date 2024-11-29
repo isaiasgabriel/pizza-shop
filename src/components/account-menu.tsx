00bed3baff0738e00ca6ce777cb10ac4dc941c88
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-11-29 16:03:18 -0300

    🔌 API connection for profile menu in the header
    It's important to note the "withCredentials" property in the axios.ts file. Without it, the code won't work because the backend will return an error due to the absence of credentials for validation.

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,5 +1,9 @@
 import { DropdownMenuLabel } from '@radix-ui/react-dropdown-menu'
+import { useQuery } from '@tanstack/react-query'
 import { Building, ChevronDown, LogOut } from 'lucide-react'
+
+import { GetManagedRestaurantAPICall } from '@/api/get-managed-restaurant'
+import { GetProfileAPICall } from '@/api/get-profile'
 
 import { Button } from './ui/button'
 import {
@@ -11,6 +15,20 @@ import {
 } from './ui/dropdown-menu'
 
 export function AccountMenu() {
+  // If you need to use the user profile data in different parts of your application,
+  // instead of making another API call to fetch it, React Query allows us to reuse this data
+  // if it has already been fetched elsewhere in the application. This is why you need
+  // the queryKey: to identify the request and enable its optimization.
+  const { data: profile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: GetProfileAPICall,
+  })
+
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: GetManagedRestaurantAPICall,
+  })
+
   return (
     <DropdownMenu>
       {/* Using `asChild` allows us to render the `Button` component as the dropdown trigger,
@@ -20,7 +38,11 @@ export function AccountMenu() {
           variant={'outline'}
           className="flex select-none items-center gap-2"
         >
-          Pizza Shop
+          {managedRestaurant?.name}
+          {/* 
+              Using "?." ensures that we safely access properties on potentially null or undefined objects.
+              Without it, if the API call fails and the profile object is null, trying to access profile.email would result in an error.
+          */}
           <ChevronDown className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
@@ -28,7 +50,7 @@ export function AccountMenu() {
       {/* Aligns the dropdown menu's right edge with the trigger's right edge, positioning it to the right. */}
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>Bashar Jackson</span>
+          <span>{profile?.name}</span>
           <span
             className="text-xs font-normal text-muted-foreground"
             /*
@@ -36,7 +58,7 @@ export function AccountMenu() {
           text-muted-foreground: a light gray tone
            */
           >
-            bashar@woo.com
+            {profile?.email}
           </span>
         </DropdownMenuLabel>
 
