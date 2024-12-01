3677da996b5d4d9869fe9bd2b348dc047a77d716
Isaías Gabriel <74021327+isaiasgabriel@users.noreply.github.com>
2024-12-01 15:23:08 -0300

    ✨ Connect 'Store Profile' modal to the API

--- src/components/store-profile-modal-dialog.tsx
+++ src/components/store-profile-modal-dialog.tsx
@@ -1,5 +1,15 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+import { GetManagedRestaurantAPICall } from '@/api/get-managed-restaurant'
+import { UpdateProfileAPICall } from '@/api/update-profile'
+
 import { Button } from './ui/button'
 import {
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
@@ -10,7 +20,52 @@ import { Input } from './ui/input'
 import { Label } from './ui/label'
 import { Textarea } from './ui/textarea'
 
+const storeProfileSchema = z.object({
+  name: z.string().min(1),
+  description: z.string(),
+})
+
+type StoreProfileSchema = z.infer<typeof storeProfileSchema>
+
 export function StoreProfileModalDialog() {
+  // At first glance, it might seem like we're making another API request.
+  // However, if you inspect the network tab in the DevTools, you'll notice
+  // that the managed restaurant data is fetched only once.
+  // As long as we use the same `queryKey`, no additional API requests will be made.
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: GetManagedRestaurantAPICall,
+  })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<StoreProfileSchema>({
+    resolver: zodResolver(storeProfileSchema),
+    values: {
+      name: managedRestaurant?.name ?? '',
+      description: managedRestaurant?.description ?? '',
+    },
+  })
+
+  const { mutateAsync: updateProfileFn } = useMutation({
+    mutationFn: UpdateProfileAPICall,
+  })
+
+  async function handleUpdateProfile(data: StoreProfileSchema) {
+    try {
+      await updateProfileFn({
+        name: data.name,
+        description: data.description,
+      })
+
+      toast.success('Profile updated with success')
+    } catch {
+      toast.error('Failed to updated profile, try again')
+    }
+  }
+
   return (
     <DialogContent>
       <DialogHeader>
@@ -20,27 +75,33 @@ export function StoreProfileModalDialog() {
         </DialogDescription>
       </DialogHeader>
 
-      <form>
+      <form onSubmit={handleSubmit(handleUpdateProfile)}>
         <div className="space-y-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label className="text-right" htmlFor="name">
               Name:
             </Label>
-            <Input className="col-span-3" id="name" />
+            <Input className="col-span-3" id="name" {...register('name')} />
           </div>
 
           <div className="grid grid-cols-4 items-center gap-4">
             <Label className="text-right" htmlFor="description">
               Description:
             </Label>
-            <Textarea className="col-span-3" id="description" />
+            <Textarea
+              className="col-span-3"
+              id="description"
+              {...register('description')}
+            />
           </div>
         </div>
         <DialogFooter>
-          <Button variant="secondary" type="button">
-            Cancel
-          </Button>
-          <Button type="submit" variant="success">
+          <DialogClose asChild>
+            <Button variant="secondary" type="button">
+              Close
+            </Button>
+          </DialogClose>
+          <Button type="submit" variant="success" disabled={isSubmitting}>
             Save
           </Button>
         </DialogFooter>
